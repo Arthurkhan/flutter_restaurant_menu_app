a1c845fcba49866f0df7327f2965ec81981bd8fa
Arthurkhan <arthurokhan@gmail.com>
2025-04-02 01:01:52 +0700

    Fix: Remove HTTP dependency from remote data source and make it throw errors when used

--- lib/data/datasources/remote_menu_data_source.dart
+++ lib/data/datasources/remote_menu_data_source.dart
@@ -1,114 +1,44 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 import '../models/menu.dart';
 import 'menu_data_source.dart';
-import '../../config/constants.dart';
 
-/// Implementation of MenuDataSource for remote API
+/// Implementation of MenuDataSource for remote API (DEPRECATED)
+/// NOTE: This class is kept for reference but should not be used.
+/// The app now operates in fully offline mode.
 class RemoteMenuDataSource implements MenuDataSource {
-  final http.Client _client;
   final String _baseUrl;
 
   RemoteMenuDataSource({
-    required http.Client client,
     String? baseUrl,
-  })  : _client = client,
-        _baseUrl = baseUrl ?? AppConstants.apiBaseUrl;
+  }) : _baseUrl = baseUrl ?? '';
 
   @override
   Future<Menu> getMenu(String menuId) async {
-    try {
-      final response = await _client.get(
-        Uri.parse('$_baseUrl/menus/$menuId'),
-        headers: {'Content-Type': 'application/json'},
-      );
-
-      if (response.statusCode == 200) {
-        return Menu.fromJson(json.decode(response.body));
-      } else {
-        throw Exception('Failed to load menu: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to connect to server: $e');
-    }
+    throw Exception('Remote data source is disabled. Use local data source instead.');
   }
 
   @override
   Future<List<Menu>> getAllMenus() async {
-    try {
-      final response = await _client.get(
-        Uri.parse('$_baseUrl/menus'),
-        headers: {'Content-Type': 'application/json'},
-      );
-
-      if (response.statusCode == 200) {
-        final List<dynamic> menuJsonList = json.decode(response.body);
-        return menuJsonList
-            .map((menuJson) => Menu.fromJson(menuJson))
-            .toList();
-      } else {
-        throw Exception('Failed to load menus: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to connect to server: $e');
-    }
+    throw Exception('Remote data source is disabled. Use local data source instead.');
   }
 
   @override
   Future<Menu> createMenu(Menu menu) async {
-    try {
-      final response = await _client.post(
-        Uri.parse('$_baseUrl/menus'),
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode(menu.toJson()),
-      );
-
-      if (response.statusCode == 201) {
-        return Menu.fromJson(json.decode(response.body));
-      } else {
-        throw Exception('Failed to create menu: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to connect to server: $e');
-    }
+    throw Exception('Remote data source is disabled. Use local data source instead.');
   }
 
   @override
   Future<Menu> updateMenu(Menu menu) async {
-    try {
-      final response = await _client.put(
-        Uri.parse('$_baseUrl/menus/${menu.id}'),
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode(menu.toJson()),
-      );
-
-      if (response.statusCode == 200) {
-        return Menu.fromJson(json.decode(response.body));
-      } else {
-        throw Exception('Failed to update menu: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to connect to server: $e');
-    }
+    throw Exception('Remote data source is disabled. Use local data source instead.');
   }
 
   @override
   Future<bool> deleteMenu(String menuId) async {
-    try {
-      final response = await _client.delete(
-        Uri.parse('$_baseUrl/menus/$menuId'),
-        headers: {'Content-Type': 'application/json'},
-      );
-
-      return response.statusCode == 204 || response.statusCode == 200;
-    } catch (e) {
-      throw Exception('Failed to connect to server: $e');
-    }
+    throw Exception('Remote data source is disabled. Use local data source instead.');
   }
 
   @override
   Future<void> saveMenu(Menu menu) async {
-    // This is a remote data source, so this method is implemented as an update
-    await updateMenu(menu);
+    throw Exception('Remote data source is disabled. Use local data source instead.');
   }
 }
